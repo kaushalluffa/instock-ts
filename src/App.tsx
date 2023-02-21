f5a18255e159478c55f02dcf3f0ab9d349ebdd34
kaushalluffa <webdevelopment852@gmail.com>
2023-02-21 17:51:23 -0500

    footer component complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,31 @@ import { Route, Routes } from "react-router-dom";
 import AddEditItem from "./components/AddEditItem/AddEditItem";
 import AddEditWarehouse from "./components/AddEditWarehouse/AddEditWarehouse";
 import AllInventory from "./components/AllInventory/AllInventory";
-import DeleteWarehouse from "./components/DeleteModal/DeleteModal";
 import Header from "./components/Header/Header";
 import ItemDescription from "./components/ItemDescription/ItemDescription";
 import SignUp from "./components/SignUp/SignUp";
 import WarehouseFull from "./components/WarehouseFull/WarehouseFull";
 import Warehouses from "./components/Warehouses/Warehouses";
-import "./App.scss";
 import Login from "./components/LogIn/Login";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Warehouses />} />
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/washington" element={<WarehouseFull />} />
         <Route path="/inventory" element={<AllInventory />} />
-        <Route path="/television" element={<ItemDescription/>}/>
-        <Route path="/edit-warehouse" element={<AddEditWarehouse/>}/>
-        <Route path="/edit-warehouse/:id" element={<AddEditWarehouse/>}/>
-        <Route path="/edit-item" element={<AddEditItem/>} />
-        <Route path="/edit-item/:id" element={<AddEditItem/>} />
+        <Route path="/television" element={<ItemDescription />} />
+        <Route path="/edit-warehouse" element={<AddEditWarehouse />} />
+        <Route path="/edit-warehouse/:id" element={<AddEditWarehouse />} />
+        <Route path="/edit-item" element={<AddEditItem />} />
+        <Route path="/edit-item/:id" element={<AddEditItem />} />
       </Routes>
-      {/* <DeleteWarehouse/> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.scss'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer__links">
+            <a href="https://linkedin.com/in/kaushalluffa">LinkedIn</a>
+            <a href="https://github.com/kaushalluffa">GitHub</a>
+            <a href="https://kaushalluffa.ca">Portfolio</a>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
